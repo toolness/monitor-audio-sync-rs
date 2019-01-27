295283967a8bf6d88394a8dac0adc7f6cf4e74df
Atul Varma <varmaa@gmail.com>
2019-01-27 15:02:19 -0500

    Hooray it works

--- src/main.rs
+++ src/main.rs
@@ -16,19 +16,30 @@ const SPECIAL_AUDIO: &str = "55P607 (NVIDIA High Definition Audio)";
 fn main() {
     use winapi::um::objbase::CoInitialize;
     use std::ptr::null_mut;
+    use std::{thread, time};
 
     hresult::validate_hresult("calling CoInitialize()", unsafe {
         CoInitialize(null_mut())
     });
 
-    println!("is_display_primary_monitor(SPECIAL_DISPLAY) = {}",
-             monitor::is_display_primary_monitor(SPECIAL_DISPLAY));
+    // This will just print out diagnostic information about our audio devices.
+    audio_devices::get_audio_device_id("");
 
-    let default_audio_id = audio_devices::get_audio_device_id(DEFAULT_AUDIO);
-    let special_audio_id = audio_devices::get_audio_device_id(SPECIAL_AUDIO);
+    let interval = time::Duration::from_secs(2);
+    let mut current_primary_monitor_is_special = None;
 
-    println!("ID for default audio is {:?}.", default_audio_id);
-    println!("ID for special audio is {:?}.", special_audio_id);
+    loop {
+        let is_special = monitor::is_display_primary_monitor(SPECIAL_DISPLAY);
 
-    policy_config::set_default_audio_playback_device(default_audio_id.unwrap().get_ptr());
+        if current_primary_monitor_is_special != Some(is_special) {
+            let name = if is_special { SPECIAL_AUDIO } else { DEFAULT_AUDIO };
+            if let Some(id) = audio_devices::get_audio_device_id(name) {
+                println!("Changing audio device to {} - {:?}.", name, id);
+                policy_config::set_default_audio_playback_device(id.get_ptr());
+            }
+            current_primary_monitor_is_special = Some(is_special);
+        }
+
+        thread::sleep(interval);
+    }
 }
