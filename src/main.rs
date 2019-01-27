2dc8f114418aac7c4957a8b35dde1faca2423317
Atul Varma <varmaa@gmail.com>
2019-01-27 14:47:04 -0500

    Get rid of window.rs."

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,12 @@
 extern crate winapi;
 
 mod monitor;
-mod window;
 mod policy_config;
 mod hresult;
 mod audio_devices;
 mod lpwstr;
 
 const SPECIAL_DISPLAY: &'static [u8] = b"\\\\.\\DISPLAY3\0";
-
-// https://docs.microsoft.com/en-us/windows/desktop/winmsg/about-window-classes
-const DIALOG_BOX_WINDOW_CLASS: &'static [u8] = b"#32770\0";
-
-const SOUND_DIALOG_TITLE: &'static [u8] = b"Sound\0";
 
 const DEFAULT_AUDIO: &str = "Speakers (Realtek High Definition Audio)";
 
@@ -29,13 +23,6 @@ fn main() {
 
     println!("is_display_primary_monitor(SPECIAL_DISPLAY) = {}",
              monitor::is_display_primary_monitor(SPECIAL_DISPLAY));
-
-    if let Some(hwnd) = window::find_window(DIALOG_BOX_WINDOW_CLASS, SOUND_DIALOG_TITLE) {
-        println!("Found sound window {:?}", hwnd);
-    } else {
-        // TODO: Run "control mmsys.cpl".
-        println!("No sound window found.");
-    }
 
     let default_audio_id = audio_devices::get_audio_device_id(DEFAULT_AUDIO);
     let special_audio_id = audio_devices::get_audio_device_id(SPECIAL_AUDIO);
