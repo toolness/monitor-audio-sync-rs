0c40da5bb9c7950199569247f82b0b25f09b6f80
Atul Varma <varmaa@gmail.com>
2019-01-27 12:50:02 -0500

    Print number of audio devices for playback.

--- src/audio_devices.rs
+++ src/audio_devices.rs
@@ -1,12 +1,20 @@
 use std::ptr::null_mut;
-use winapi::um::mmdeviceapi::{IMMDeviceEnumerator, CLSID_MMDeviceEnumerator};
+use winapi::um::mmdeviceapi::{
+    IMMDeviceEnumerator,
+    CLSID_MMDeviceEnumerator,
+    IMMDeviceCollection,
+    eRender,
+    DEVICE_STATE_ACTIVE
+};
 use winapi::um::combaseapi::{CoCreateInstance, CLSCTX_ALL};
 use winapi::Interface;
+
+use super::hresult::validate_hresult;
 
 pub fn get_audio_devices() {
     let mut enumerator_ptr = null_mut();
 
-    super::hresult::validate_hresult("obtaining MMDeviceEnumerator", unsafe {
+    validate_hresult("obtaining MMDeviceEnumerator", unsafe {
         CoCreateInstance(
             &CLSID_MMDeviceEnumerator,
             null_mut(),
@@ -17,6 +25,24 @@ pub fn get_audio_devices() {
     });
 
     let enumerator = enumerator_ptr as *mut IMMDeviceEnumerator;
+
+    let mut collection: *mut IMMDeviceCollection = null_mut();
+
+    validate_hresult("calling EnumAudioEndpoints()", unsafe {
+        (*enumerator).EnumAudioEndpoints(
+            eRender,
+            DEVICE_STATE_ACTIVE,
+            &mut collection
+        )
+    });
+
+    let mut count: u32 = 0;
+
+    validate_hresult("calling GetCount()", unsafe { (*collection).GetCount(&mut count) });
+
+    println!("You have {} audio devices for playback.", count);
+
+    unsafe { (*enumerator).Release(); }
 
     unsafe { (*enumerator).Release(); }
 }
