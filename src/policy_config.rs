684d880ef05b9d19257e46275b483241d785f476
Atul Varma <varmaa@gmail.com>
2019-01-27 12:27:00 -0500

    Release the policy config after using it.

--- src/policy_config.rs
+++ src/policy_config.rs
@@ -24,7 +24,7 @@ interface IPolicyConfig(IPolicyConfigVtbl): IUnknown(IUnknownVtbl) {
 DEFINE_GUID!{CPolicyConfigClient, 0x870af99c, 0x171d, 0x4f9e, 0xaf, 0x0d, 0xe6, 0x3d, 0xf4, 0x0c, 0x2b, 0xc9}
 
 pub fn get_policy_config_client() {
-    let mut policy_config = null_mut();
+    let mut policy_config_ptr = null_mut();
 
     super::hresult::validate_hresult("obtaining CPolicyConfigClient", unsafe {
         CoCreateInstance(
@@ -32,7 +32,11 @@ pub fn get_policy_config_client() {
             null_mut(),
             CLSCTX_ALL,
             &IPolicyConfig::uuidof(),
-            &mut policy_config
+            &mut policy_config_ptr
         )
     });
+
+    let policy_config = policy_config_ptr as *mut IPolicyConfig;
+    
+    unsafe { (*policy_config).Release() };
 }
