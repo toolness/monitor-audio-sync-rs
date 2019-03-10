6347fe61f14e38ccbd4454999e447721ee9e6ab8
Atul Varma <varmaa@gmail.com>
2019-03-10 17:27:57 -0400

    Use windows_subsystem.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// Uncomment the following line to make it possible to
+// run the program in console mode.
+#![windows_subsystem = "windows"]
+
 #[macro_use(RIDL, DEFINE_GUID, DEFINE_PROPERTYKEY)]
 extern crate winapi;
 
